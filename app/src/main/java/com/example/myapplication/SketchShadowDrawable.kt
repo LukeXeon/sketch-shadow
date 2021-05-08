3134e7aa310424c5fbb6f44133b55ce0833ed64a
发光的半导体 <694014820@qq.com>
2021-05-09 05:17:14 +0800

    m

--- app/src/main/java/com/example/myapplication/SketchShadowDrawable.kt
+++ app/src/main/java/com/example/myapplication/SketchShadowDrawable.kt
@@ -58,6 +58,30 @@ class SketchShadowDrawable private constructor(
     }
 
     private companion object ViewCompat {
+
+        private val FLAG_CLIP_TO_PADDING by lazy {
+            ViewGroup::class.java.getDeclaredField("FLAG_CLIP_TO_PADDING")
+                .apply {
+                    isAccessible = true
+                }.getInt(null)
+        }
+
+        private val mGroupFlagsField by lazy {
+            ViewGroup::class.java.getDeclaredField("mGroupFlags")
+                .apply {
+                    isAccessible = true
+                }
+        }
+
+        private val ViewGroup.mGroupFlags: Int
+            get() {
+                return mGroupFlagsField.getInt(this)
+            }
+
+        private fun ViewGroup.hasBooleanFlag(flag: Int): Boolean {
+            return mGroupFlags and flag == flag
+        }
+
         var View.clipToOutlineCompat: Boolean
             get() {
                 return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -76,13 +100,11 @@ class SketchShadowDrawable private constructor(
                 return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     this.clipToPadding
                 } else {
-                    return false
+                    return hasBooleanFlag(FLAG_CLIP_TO_PADDING)
                 }
             }
             set(value) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    this.clipToPadding = value
-                }
+                this.clipToPadding = value
             }
     }
 
