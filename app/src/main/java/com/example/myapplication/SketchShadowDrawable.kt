f88f3ae585a9de03cff2130ad35ca69f2c7f4401
发光的半导体 <694014820@qq.com>
2021-05-09 04:07:42 +0800

    m

--- app/src/main/java/com/example/myapplication/SketchShadowDrawable.kt
+++ app/src/main/java/com/example/myapplication/SketchShadowDrawable.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.Drawable
@@ -7,22 +8,27 @@ import android.graphics.drawable.NinePatchDrawable
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.os.Process
 import android.util.Base64
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.annotation.*
+import androidx.annotation.ColorInt
+import androidx.annotation.Keep
+import androidx.annotation.MainThread
+import androidx.annotation.Px
 import org.json.JSONObject
 import java.lang.reflect.Modifier
 import java.util.concurrent.*
+import kotlin.concurrent.thread
 import kotlin.math.max
 import kotlin.math.min
 
 @Suppress("unused")
 class SketchShadowDrawable private constructor(
     margin: Rect,
-    @Suppress("unused") val memorySize: Int,
+    @Suppress("unused") val bitmap: Bitmap,
     private val ninePatchDrawable: NinePatchDrawable,
 ) : Drawable(), Drawable.Callback {
 
@@ -30,8 +36,54 @@ class SketchShadowDrawable private constructor(
     val margin: Rect = margin
         get() = Rect(field)
 
+
     override fun draw(canvas: Canvas) {
+        var callback = callback
+        while (callback is Drawable) {
+            callback = callback.callback
+        }
+        if (callback is View) {
+            val parent = callback.parent
+            if (parent is ViewGroup) {
+                if (parent.clipChildren || parent.clipToOutlineCompat || parent.clipToOutlineCompat) {
+                    parent.clipChildren = false
+                    parent.clipToPaddingCompat = false
+                    parent.clipToOutlineCompat = false
+                    invalidateSelf()
+                    return
+                }
+            }
+        }
         ninePatchDrawable.draw(canvas)
+    }
+
+    private companion object ViewCompat {
+        var View.clipToOutlineCompat: Boolean
+            get() {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    this.clipToOutline
+                } else {
+                    false
+                }
+            }
+            set(value) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    this.clipToOutline = value
+                }
+            }
+        var ViewGroup.clipToPaddingCompat: Boolean
+            get() {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    this.clipToPadding
+                } else {
+                    return false
+                }
+            }
+            set(value) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    this.clipToPadding = value
+                }
+            }
     }
 
     override fun setBounds(left: Int, top: Int, right: Int, bottom: Int) {
@@ -65,24 +117,6 @@ class SketchShadowDrawable private constructor(
         return ninePatchDrawable.opacity
     }
 
-    override fun onBoundsChange(bounds: Rect?) {
-        super.onBoundsChange(bounds)
-        var callback = callback
-        while (callback is Drawable) {
-            callback = callback.callback
-        }
-        if (callback is View) {
-            val parent = callback.parent
-            if (parent is ViewGroup) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    parent.clipToOutline = false
-                }
-                parent.clipChildren = false
-                parent.clipToPadding = false
-            }
-        }
-    }
-
     override fun invalidateDrawable(who: Drawable) {
         callback?.invalidateDrawable(who)
     }
@@ -95,27 +129,36 @@ class SketchShadowDrawable private constructor(
         callback?.unscheduleDrawable(who, what)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     class Factory(context: Context) {
         private val webkit = WebView(context.applicationContext)
         private val mainHandler = Handler(Looper.getMainLooper())
         private val executor: Executor
-        private val pendingActions = ArrayList<Pair<String, Callback>>()
-        private var isLoadFinished = false
+        private var needPendingTasks: ArrayList<Pair<String, RequestCallback>>? = ArrayList()
 
         init {
             val nThread = min(4, max(2, Runtime.getRuntime().availableProcessors()))
             executor = ThreadPoolExecutor(
                 0, nThread,
                 60, TimeUnit.MILLISECONDS,
-                LinkedBlockingQueue()
+                LinkedBlockingQueue(),
+                ThreadFactory {
+                    thread(
+                        priority = Process.THREAD_PRIORITY_DEFAULT,
+                        block = { it.run() }
+                    )
+                }
             )
+            webkit.settings.javaScriptEnabled = true
             webkit.webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView?, url: String?) {
-                    isLoadFinished = true
-                    pendingActions.forEach {
-                        createDrawableAsync0(it.first, it.second)
+                    val tasks = needPendingTasks
+                    if (tasks != null) {
+                        tasks.forEach {
+                            requestByWebView(it.first, it.second)
+                        }
+                        needPendingTasks = null
                     }
-                    pendingActions.clear()
                 }
             }
             webkit.loadUrl("file:///android_asset/webkit_shadow_renderer/index.html")
@@ -126,7 +169,6 @@ class SketchShadowDrawable private constructor(
             imageData: ByteArray
         ): SketchShadowDrawable {
             val bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
-            val memorySize = bitmap.width * bitmap.height * Int.SIZE_BYTES
             return SketchShadowDrawable(
                 Rect(
                     margin[0],
@@ -134,7 +176,7 @@ class SketchShadowDrawable private constructor(
                     margin[2],
                     margin[3]
                 ),
-                memorySize,
+                bitmap,
                 NinePatchDrawable(
                     webkit.resources,
                     NinePatch(bitmap, bitmap.ninePatchChunk)
@@ -142,28 +184,34 @@ class SketchShadowDrawable private constructor(
             )
         }
 
-        private fun createDrawableAsync0(json: String, callback: Callback) {
+        private fun requestByWebView(json: String, callback: RequestCallback) {
             webkit.evaluateJavascript("createNinePatch('$json')") {
                 executor.execute {
                     val result = JSONObject(it)
-                    val marginArray = result.getJSONArray("margin")
-                    val imageData = result.getString("imageData")
-                    val imageBytes = Base64.decode(imageData, Base64.DEFAULT)
-                    val drawable = createDrawable((0..3).map { index ->
-                        marginArray.getInt(index)
-                    }.toIntArray(), imageBytes)
-                    mainHandler.post { callback.onCreated(drawable) }
+                    if (result.has("error")) {
+                        val error = result.getString("error")
+                        val exception = UnsupportedOperationException(error)
+                        mainHandler.post { callback.onError(exception) }
+                    } else {
+                        val marginArray = result.getJSONArray("margin")
+                        val imageData = result.getString("imageData")
+                        val imageBytes = Base64.decode(imageData, Base64.DEFAULT)
+                        val drawable = createDrawable((0..3).map { index ->
+                            marginArray.getInt(index)
+                        }.toIntArray(), imageBytes)
+                        mainHandler.post { callback.onResponse(drawable) }
+                    }
                 }
             }
         }
 
         @MainThread
-        fun createDrawableAsync(
-            options: Options,
-            callback: Callback
+        fun request(
+            options: RequestOptions,
+            callback: RequestCallback
         ) {
             val json = JSONObject(
-                Options::class.java.declaredFields
+                RequestOptions::class.java.declaredFields
                     .map {
                         it
                     }.filter {
@@ -172,68 +220,58 @@ class SketchShadowDrawable private constructor(
                     .map { it.name to it.get(options) }
                     .toMap()
             ).toString()
-            if (isLoadFinished) {
-                createDrawableAsync0(json, callback)
+            val tasks = needPendingTasks
+            if (tasks == null) {
+                requestByWebView(json, callback)
             } else {
-                pendingActions.add(json to callback)
-            }
-        }
-
-        interface Callback {
-            fun onCreated(drawable: SketchShadowDrawable)
-        }
-
-        @Keep
-        data class Options(
-            @Px
-            var roundLeftTop: Int = 12,
-            @Px
-            var roundRightTop: Int = 12,
-            @Px
-            var roundLeftBottom: Int = 12,
-            @Px
-            var roundRightBottom: Int = 12,
-            @Px
-            var shadowBlur: Int = 20,
-            @ColorInt
-            var shadowColor: Int = Color.parseColor("#757575"),
-            @Px
-            var shadowDx: Int = 0,
-            @Px
-            var shadowDy: Int = 0,
-            @ColorInt
-            var backgroundFillColor: Int = Color.TRANSPARENT,
-            @ColorInt
-            var fillColor: Int = Color.TRANSPARENT,
-            @ColorInt
-            var outlineColor: Int = Color.TRANSPARENT,
-            @Px
-            var outlineWidth: Int = 0,
-            @Px
-            var width: Int = 200,
-            @Px
-            var height: Int = 200,
-            @Px
-            var paddingLeft: Int = UNSET_PADDING,
-            @Px
-            var paddingRight: Int = UNSET_PADDING,
-            @Px
-            var paddingTop: Int = UNSET_PADDING,
-            @Px
-            var paddingBottom: Int = UNSET_PADDING,
-            @FloatRange(from = 0.0, to = 1.0)
-            var contentRightBegin: Float = 0f,
-            @FloatRange(from = 0.0, to = 1.0)
-            var contentRightEnd: Float = 0f,
-            @FloatRange(from = 0.0, to = 1.0)
-            var contentBottomBegin: Float = 0f,
-            @FloatRange(from = 0.0, to = 1.0)
-            var contentBottomEnd: Float = 0f,
-        ) : Cloneable {
-            companion object {
-                const val UNSET_PADDING = -1
+                tasks.add(json to callback)
             }
         }
     }
 
+    interface RequestCallback {
+        fun onResponse(drawable: SketchShadowDrawable)
+
+        fun onError(e: Throwable)
+    }
+
+    @Keep
+    data class RequestOptions(
+        @Px
+        var roundLeftTop: Int = 12,
+        @Px
+        var roundRightTop: Int = 12,
+        @Px
+        var roundLeftBottom: Int = 12,
+        @Px
+        var roundRightBottom: Int = 12,
+        @Px
+        var shadowBlur: Int = 20,
+        @ColorInt
+        var shadowColor: Int = Color.parseColor("#757575"),
+        @Px
+        var shadowDx: Int = 0,
+        @Px
+        var shadowDy: Int = 0,
+        @ColorInt
+        var backgroundFillColor: Int = Color.TRANSPARENT,
+        @ColorInt
+        var fillColor: Int = Color.TRANSPARENT,
+        @ColorInt
+        var outlineColor: Int = Color.TRANSPARENT,
+        @Px
+        var outlineWidth: Int = 0,
+        @Px
+        var paddingLeft: Int = UNSET_PADDING,
+        @Px
+        var paddingRight: Int = UNSET_PADDING,
+        @Px
+        var paddingTop: Int = UNSET_PADDING,
+        @Px
+        var paddingBottom: Int = UNSET_PADDING
+    ) : Cloneable {
+        companion object {
+            const val UNSET_PADDING = -1
+        }
+    }
 }
