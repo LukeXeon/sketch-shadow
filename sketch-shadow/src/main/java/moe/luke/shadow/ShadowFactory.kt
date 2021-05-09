792fb773384affdee069394bcf8ebb38a86d145c
发光的半导体 <694014820@qq.com>
2021-05-09 23:18:34 +0800

    m

--- sketch-shadow/src/main/java/moe/luke/shadow/ShadowFactory.kt
+++ sketch-shadow/src/main/java/moe/luke/shadow/ShadowFactory.kt
@@ -7,79 +7,93 @@ import android.graphics.NinePatch
 import android.graphics.Rect
 import android.graphics.drawable.NinePatchDrawable
 import android.util.Base64
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.annotation.Keep
+import kotlinx.coroutines.*
 import org.json.JSONObject
 import java.lang.reflect.Modifier
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-@SuppressLint("SetJavaScriptEnabled")
+@SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
 class ShadowFactory(context: Context) {
 
-    private inner class PendingTask(
-        private val continuation: Continuation<ShadowDrawable>,
-        private val input: String
-    ) {
-        fun execute() {
-            MainScope().launch {
-                continuation.resume(createFromWebView(input))
+    private data class PendingTask(
+        val input: JSONObject,
+        val continuation: Continuation<ShadowDrawable>
+    )
+
+    private inner class EventHandler : WebViewClient() {
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            val paddingTasks = needPendingTasks
+            if (paddingTasks != null) {
+                for ((input, continuation) in paddingTasks) {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        continuation.resume(scheduleTask(input))
+                    }
+                }
+                needPendingTasks = null
+            }
+        }
+
+        @Keep
+        @JavascriptInterface
+        fun onResponse(output: String, id: String) {
+            val continuation = tasks[id] ?: return
+            tasks.remove(id)
+            GlobalScope.launch(Dispatchers.Default) {
+                val json = JSONObject(output)
+                val drawable = if (json.has("error")) {
+                    throw UnsupportedOperationException(json.getString("error"))
+                } else {
+                    val margin = json.getJSONArray("margin")
+                    val imageData = json.getString("imageData")
+                    val imageBuffer = Base64.decode(imageData, Base64.DEFAULT)
+                    val bitmap = BitmapFactory.decodeByteArray(imageBuffer, 0, imageBuffer.size)
+                    val chunk = NinePatchChunk.create(bitmap).serializedChunk
+                    ShadowDrawable(
+                        Rect(
+                            margin.getInt(0),
+                            margin.getInt(1),
+                            margin.getInt(2),
+                            margin.getInt(3)
+                        ),
+                        bitmap,
+                        chunk,
+                        NinePatchDrawable(
+                            webkit.resources,
+                            NinePatch(bitmap, chunk)
+                        )
+                    )
+                }
+                continuation.resume(drawable)
             }
         }
     }
 
     private val webkit = WebView(context.applicationContext)
+    private val tasks = HashMap<String, Continuation<ShadowDrawable>>()
     private var needPendingTasks: ArrayList<PendingTask>? = ArrayList()
 
     init {
+        val handler = EventHandler()
         webkit.settings.javaScriptEnabled = true
-        webkit.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                val tasks = needPendingTasks
-                if (tasks != null) {
-                    for (task in tasks) {
-                        task.execute()
-                    }
-                    needPendingTasks = null
-                }
-            }
-        }
+        webkit.webViewClient = handler
+        webkit.addJavascriptInterface(handler, "_handler_")
         webkit.loadUrl("file:///android_asset/webkit_shadow_renderer/index.html")
     }
 
-    private suspend fun createFromWebView(input: String): ShadowDrawable {
-        val result = webkit.evaluateJavascript("createNinePatch('$input')")
-        return withContext(Dispatchers.Default) {
-            val json = JSONObject(result)
-            if (json.has("error")) {
-                throw UnsupportedOperationException(json.getString("error"))
-            } else {
-                val margin = json.getJSONArray("margin")
-                val imageData = json.getString("imageData")
-                val imageBuffer = Base64.decode(imageData, Base64.DEFAULT)
-                val bitmap = BitmapFactory.decodeByteArray(imageBuffer, 0, imageBuffer.size)
-                val chunk = NinePatchChunk.create(bitmap).serializedChunk
-                ShadowDrawable(
-                    Rect(
-                        margin.getInt(0),
-                        margin.getInt(1),
-                        margin.getInt(2),
-                        margin.getInt(3)
-                    ),
-                    bitmap,
-                    chunk,
-                    NinePatchDrawable(
-                        webkit.resources,
-                        NinePatch(bitmap, chunk)
-                    )
-                )
-            }
-        }
+    private suspend fun scheduleTask(input: JSONObject): ShadowDrawable {
+        val id = UUID.randomUUID().toString()
+        webkit.evaluateJavascript("createNinePatch('$input','$id')")
+        return suspendCoroutine { tasks[id] = it }
     }
 
     /**
@@ -88,22 +102,20 @@ class ShadowFactory(context: Context) {
     suspend fun newDrawable(
         options: ShadowOptions
     ): ShadowDrawable {
-        val json = JSONObject(
+        val input = JSONObject(
             ShadowOptions::class.java.declaredFields
                 .filter {
                     !Modifier.isStatic(it.modifiers)
                 }
                 .map { it.name to it.apply { isAccessible = true }.get(options) }
                 .toMap()
-        ).toString()
+        )
         return withContext(Dispatchers.Main) {
-            val tasks = needPendingTasks
-            if (tasks == null) {
-                createFromWebView(json)
+            val paddingTasks = needPendingTasks
+            if (paddingTasks == null) {
+                scheduleTask(input)
             } else {
-                suspendCoroutine {
-                    tasks.add(PendingTask(it, json))
-                }
+                suspendCoroutine { paddingTasks.add(PendingTask(input, it)) }
             }
         }
     }
