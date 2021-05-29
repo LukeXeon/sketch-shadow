8f7641280bd2005a526d2b52c4fb89c2a0bf1ea6
发光的半导体 <694014820@qq.com>
2021-05-29 20:18:50 +0800

    m

--- sketch-shadow/src/main/java/org/gcta/shadow/AppCompatWebView.kt
+++ sketch-shadow/src/main/java/org/gcta/shadow/AppCompatWebView.kt
@@ -1,85 +1,254 @@
 package org.gcta.shadow
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Build
-import android.os.Handler
-import android.os.Looper
+import android.os.Message
 import android.util.AttributeSet
-import android.webkit.JavascriptInterface
-import android.webkit.ValueCallback
-import android.webkit.WebView
-import androidx.annotation.WorkerThread
-import java.lang.ref.WeakReference
+import android.view.View
+import android.webkit.*
+import com.google.gson.Gson
+import com.google.gson.JsonNull
+import com.google.gson.JsonObject
 import java.util.*
 import kotlin.collections.HashMap
-import androidx.annotation.Keep as AndroidXKeep
-import proguard.annotation.Keep as ProguardKeep
 
 
-@SuppressLint("AddJavascriptInterface")
 open class AppCompatWebView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : WebView(context, attrs, defStyleAttr) {
 
-    private val delegate by lazy { AppCompatDelegate(this) }
+    private val delegate by lazy { AppCompatDelegate() }
 
     init {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            super.addJavascriptInterface(
-                delegate,
-                delegate.toString()
-            )
+            super.setWebChromeClient(delegate)
+        }
+    }
+
+    override fun setWebChromeClient(client: WebChromeClient?) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            delegate.client = client
+        } else {
+            super.setWebChromeClient(client)
         }
     }
 
     override fun evaluateJavascript(script: String, resultCallback: ValueCallback<String?>?) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            delegate.evaluateJavascript(script, resultCallback)
+            delegate.evaluateJavascript(this, script, resultCallback)
         } else {
             super.evaluateJavascript(script, resultCallback)
         }
     }
 
-    private class AppCompatDelegate(webView: WebView) {
-        private val webView = WeakReference(webView)
-        private val mainThread = Handler(Looper.getMainLooper())
+    private class AppCompatDelegate : WebChromeClient() {
+        var client: WebChromeClient? = null
         private val callbacks = HashMap<String, ValueCallback<String?>>()
+        private val prefix = "AppCompatJsResponse@${hashCode()}="
 
-        fun evaluateJavascript(script: String, callback: ValueCallback<String?>?) {
-            val view = webView.get() ?: return
+        fun evaluateJavascript(
+            webView: WebView,
+            script: String,
+            callback: ValueCallback<String?>?
+        ) {
             val id = UUID.randomUUID().toString()
             var internalScript = "(function(){$script})();"
-            val manager = toString()
             internalScript = if (callback == null) {
                 internalScript
             } else {
                 callbacks[id] = callback
-                """(function(){
+                """(function () {
+                        var ex = null;
+                        var result = null;
                         try {
-                            var result = $internalScript
-                            $manager.onResponse('$id',result?JSON.stringify(result):null);
+                            result = $internalScript
                         } catch (e) {
-                            $manager.onResponse('$id',null);
-                            throw e;
+                            ex = e;
                         }
-                })();""".trimIndent()
+                        window.prompt('$prefix' + JSON.stringify({id: '$id', data: result}));
+                        if (ex) {
+                            throw ex;
+                        }
+                   })();"""
             }
-            view.loadUrl("javascript:$internalScript")
+            webView.loadUrl("javascript:$internalScript")
         }
 
-        @AndroidXKeep
-        @ProguardKeep
-        @WorkerThread
-        @JavascriptInterface
-        fun onResponse(callbackId: String, result: String?) {
-            mainThread.post {
-                callbacks.remove(callbackId)?.onReceiveValue(result)
+        override fun onJsPrompt(
+            view: WebView?,
+            url: String?,
+            message: String?,
+            defaultValue: String?,
+            result: JsPromptResult
+        ): Boolean {
+            if (!message.isNullOrEmpty() && message.startsWith(prefix)) {
+                val string = message.substring(prefix.length)
+                val obj = gson.fromJson(string, JsonObject::class.java)
+                val callback = callbacks.remove(obj.get("id").asString)
+                if (callback != null) {
+                    val dataElement = obj.get("data")
+                    if (dataElement == null || dataElement == JsonNull.INSTANCE) {
+                        callback.onReceiveValue(null)
+                    } else {
+                        callback.onReceiveValue(obj.get("data").asString)
+                    }
+                }
+                result.confirm()
+                return true
+            } else {
+                return super.onJsPrompt(view, url, message, defaultValue, result)
             }
         }
 
-        override fun toString(): String {
-            return javaClass.simpleName + hashCode()
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            client?.onProgressChanged(view, newProgress)
+        }
+
+        override fun onReceivedTitle(view: WebView?, title: String?) {
+            client?.onReceivedTitle(view, title)
+        }
+
+        override fun onReceivedIcon(view: WebView?, icon: Bitmap?) {
+            client?.onReceivedIcon(view, icon)
+        }
+
+        override fun onReceivedTouchIconUrl(view: WebView?, url: String?, precomposed: Boolean) {
+            client?.onReceivedTouchIconUrl(view, url, precomposed)
+        }
+
+        override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+            client?.onShowCustomView(view, callback)
+        }
+
+        override fun onShowCustomView(
+            view: View?,
+            requestedOrientation: Int,
+            callback: CustomViewCallback?
+        ) {
+            client?.onShowCustomView(view, requestedOrientation, callback)
+        }
+
+        override fun onHideCustomView() {
+            client?.onHideCustomView()
+        }
+
+        override fun onCreateWindow(
+            view: WebView?,
+            isDialog: Boolean,
+            isUserGesture: Boolean,
+            resultMsg: Message?
+        ): Boolean {
+            return client?.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+                ?: super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+        }
+
+        override fun onRequestFocus(view: WebView?) {
+            client?.onRequestFocus(view)
+        }
+
+        override fun onCloseWindow(window: WebView?) {
+            client?.onCloseWindow(window)
+        }
+
+        override fun onJsAlert(
+            view: WebView?,
+            url: String?,
+            message: String?,
+            result: JsResult?
+        ): Boolean {
+            return client?.onJsAlert(view, url, message, result)
+                ?: super.onJsAlert(view, url, message, result)
+        }
+
+        override fun onJsConfirm(
+            view: WebView?,
+            url: String?,
+            message: String?,
+            result: JsResult?
+        ): Boolean {
+            return client?.onJsConfirm(view, url, message, result)
+                ?: super.onJsConfirm(view, url, message, result)
+        }
+
+        override fun onJsBeforeUnload(
+            view: WebView?,
+            url: String?,
+            message: String?,
+            result: JsResult?
+        ): Boolean {
+            return client?.onJsBeforeUnload(view, url, message, result)
+                ?: super.onJsBeforeUnload(view, url, message, result)
+        }
+
+        override fun onExceededDatabaseQuota(
+            url: String?,
+            databaseIdentifier: String?,
+            quota: Long,
+            estimatedDatabaseSize: Long,
+            totalQuota: Long,
+            quotaUpdater: WebStorage.QuotaUpdater?
+        ) {
+            client?.onExceededDatabaseQuota(
+                url,
+                databaseIdentifier,
+                quota,
+                estimatedDatabaseSize,
+                totalQuota,
+                quotaUpdater
+            )
+        }
+
+        override fun onReachedMaxAppCacheSize(
+            requiredStorage: Long,
+            quota: Long,
+            quotaUpdater: WebStorage.QuotaUpdater?
+        ) {
+            client?.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater)
+        }
+
+        override fun onGeolocationPermissionsShowPrompt(
+            origin: String?,
+            callback: GeolocationPermissions.Callback?
+        ) {
+            client?.onGeolocationPermissionsShowPrompt(origin, callback)
+        }
+
+        override fun onGeolocationPermissionsHidePrompt() {
+            client?.onGeolocationPermissionsHidePrompt()
+        }
+
+        override fun onJsTimeout(): Boolean {
+            return client?.onJsTimeout()
+                ?: super.onJsTimeout()
+        }
+
+        override fun onConsoleMessage(message: String?, lineNumber: Int, sourceID: String?) {
+            client?.onConsoleMessage(message, lineNumber, sourceID)
+        }
+
+        override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+            return client?.onConsoleMessage(consoleMessage)
+                ?: super.onConsoleMessage(consoleMessage)
+        }
+
+        override fun getDefaultVideoPoster(): Bitmap? {
+            return client?.defaultVideoPoster
+                ?: super.getDefaultVideoPoster()
+        }
+
+        override fun getVideoLoadingProgressView(): View? {
+            return client?.videoLoadingProgressView
+                ?: super.getVideoLoadingProgressView()
+        }
+
+        override fun getVisitedHistory(callback: ValueCallback<Array<String>>?) {
+            client?.getVisitedHistory(callback)
+        }
+
+        companion object {
+            private val gson by lazy { Gson() }
         }
     }
+    
 }
